a97d49de18f7952f597f6a8f7e0c2391f022702f
arie <arieinjector@gmail.com>
2022-04-16 23:03:43 +0700

    only mobile

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+<div class="fixed left-0 top-0 w-screen h-screen hidden bg-[#091945] z-50 font-pupylinux text-white lg:flex justify-center items-center text-3xl">
+  <p>Sorry this site mobile only</p>
+</div>
 <Navbar />
   <Bar v-if="$route.name != 'surah'" />
   <router-view></router-view>
